35e159f7c0a351ea1ead5db0edc5862bcc5755d8
정기환 <joelkh784@gmail.com>
2024-04-15 17:06:42 +0900

    ✨ Feat: 프로젝트 초기 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,73 @@
-import type { Config } from "tailwindcss";
+/** @type {import('tailwindcss').Config} */
 
-const config: Config = {
+const rem0_10 = { ...Array.from(Array(11)).map((_, i) => `${i / 10}rem`) };
+const rem0_100 = { ...Array.from(Array(101)).map((_, i) => `${i / 10}rem`) };
+const rem0_800 = { ...Array.from(Array(801)).map((_, i) => `${i / 10}rem`) };
+
+module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      borderWidth: rem0_10,
+      borderRadius: rem0_100,
+      fontSize: rem0_100,
+      lineHeight: rem0_100,
+      minWidth: rem0_800,
+      minHeight: rem0_800,
+      spacing: rem0_800,
+      colors: {
+        blue: {
+          '3f': '#3F72AF',
+          76: '#76A5EA',
+          db: '#DBE2EF',
+        },
+        gray: {
+          78: '#787486',
+          '9f': '#9FA6B2',
+          d9: '#D9D9D9',
+          ee: '#EEEEEE',
+          fa: '#FAFAFA',
+        },
+        black: {
+          DEFAULT: '#000',
+          17: '#171717',
+          33: '#333236',
+          '4b': '#4B4B4B',
+          overlay: 'rgba(0, 0, 0, 0.70)', // 모달창 뒷 배경
+        },
+        red: '#D6173A',
+        green: '#7AC555',
+        purple: '#760DDE',
+        orange: '#FFA500',
+        pink: '#E876EA',
+        violet: {
+          DEFAULT: '#5534DA',
+          '8%': '#F1EFFD',
+        },
+        white: '#FFF',
+        'beige-f9': '#F9F7F7',
+      },
+      screens: {
+        tablet: { max: '1199px' },
+        mobile: { max: '767px' },
+      },
+      fontFamily: {
+        sans: ['Pretendard', 'sans-serif'],
+      },
+      zIndex: {
+        DEFAULT: '1',
+        dropdown: '200',
+        sticky: '400',
+        popover: '600',
+        overlay: '800',
+        modal: '1000',
+        toast: '1200',
       },
     },
   },
   plugins: [],
 };
-export default config;
