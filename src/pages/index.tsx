3e64ea9cc2b31e5379d1e9eaacc45e81e5351db9
정기환 <joelkh784@gmail.com>
2024-04-16 16:16:22 +0900

    ✨ Feat: landing page footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import Content from '../components/landing/Content';
+import Footer from '../components/landing/Footer';
 
 const Home = () => {
   return (
     <>
       {/* <Header /> */}
       <Content />
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 };
