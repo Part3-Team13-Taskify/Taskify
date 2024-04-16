558d82702843b50525e41b64da588e46fb860d5e
Harry K <155596644+harrykotter@users.noreply.github.com>
2024-04-16 15:19:26 +0900

    🎨 Refactor: 카드 컴포넌트 UI 최적화
    카드 컴포넌트 세부 단위 조정 및 날짜 입력 편의성을 고려한 date format 함수 추가

--- src/components/ExampleCard.tsx
+++ src/components/ExampleCard.tsx
@@ -5,7 +5,11 @@ const ExampleCard = () => {
   const title = '새로운 일정 관리 Taskify';
   const date = '2022.12.31';
   const profile = 'assets/chip/ellipseDefault.svg';
-  return <Card src={imgSource} title={title} date={date} profile={profile}></Card>;
+  return (
+    <div className="m-20">
+      <Card src={imgSource} title={title} date={date} profile={profile}></Card>
+    </div>
+  );
 };
 
 export default ExampleCard;
