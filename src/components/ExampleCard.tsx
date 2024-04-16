a91d9a710c50c7ac76140caff797e7a2f12089c0
Harry K <155596644+harrykotter@users.noreply.github.com>
2024-04-16 15:36:01 +0900

    ✅ Refactor: 날짜 예시 조정
    날짜 예시를 ISO 8601 표준 형식으로 조정

--- src/components/ExampleCard.tsx
+++ src/components/ExampleCard.tsx
@@ -1,9 +1,9 @@
-import { Card } from './common/Card';
+import Card from './common/Card';
 
 const ExampleCard = () => {
   const imgSource = 'assets/card/desktop/card_image1.svg';
   const title = '새로운 일정 관리 Taskify';
-  const date = '2022.12.31';
+  const date = '2024-04-16T06:28:59.964Z';
   const profile = 'assets/chip/ellipseDefault.svg';
   return (
     <div className="m-20">
