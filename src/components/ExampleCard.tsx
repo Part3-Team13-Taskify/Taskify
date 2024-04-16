43b6f8a3a5fec90a2c51c7b01103cb2a063f4a32
Harry K <155596644+harrykotter@users.noreply.github.com>
2024-04-16 13:50:59 +0900

    💄 Refactor: 카드 컴포넌트 반응형 구현
    화면 너비에 따른 반응형 구현 - md, xl 단위

--- src/components/ExampleCard.tsx
+++ src/components/ExampleCard.tsx
@@ -4,7 +4,8 @@ const ExampleCard = () => {
   const imgSource = 'assets/card/desktop/card_image1.svg';
   const title = '새로운 일정 관리 Taskify';
   const date = '2022.12.31';
-  return <Card src={imgSource} title={title} date={date}></Card>;
+  const profile = 'assets/chip/ellipseDefault.svg';
+  return <Card src={imgSource} title={title} date={date} profile={profile}></Card>;
 };
 
 export default ExampleCard;
--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -1,37 +1,37 @@
 import Image from 'next/image';
 
-export const Card = ({ src, title, date }: { src?: string; title: string; date: string }) => {
+export const Card = ({ src, title, date, profile }: { src?: string; title: string; date: string; profile: string }) => {
+  const titleShort = title.length > 30 ? title.slice(0, 29) + '...' : title;
+
   return (
-    <>
-      <div className="max-w-450 md:max-w-full xl:max-w-450 rounded-6 py-20 px-20 m-10 border-1 border-gray-d9 bg-white hover:border-violet">
-        <div className="flex flex-col md:flex-row xl:flex-col justify:start gap-12">
-          {!!src && (
-            <Image
-              src={src}
-              width={450}
-              height={262}
-              alt="Card Image"
-              className="rounded-6 w-full md:w-90 xl:w-full h-auto"
-            />
-          )}
-          <div className="w-full flex flex-col gap-10">
-            <span className="text-black font-medium">{title}</span>
-            <div className="flex flex-col md:flex-row xl:flex-col gap-10">
-              <div className="flex gap-6">
-                <Image src="assets/card/exampleChip/large1.svg" width={44} height={22} alt="chip" className="w-auto" />
-                <Image src="assets/card/exampleChip/large3.svg" width={44} height={22} alt="chip" className="w-auto" />
+    <div className="max-w-450 md:max-w-full xl:max-w-450 rounded-6 py-20 px-20 m-10 border-1 border-gray-d9 bg-white hover:border-violet">
+      <div className="flex flex-col md:flex-row xl:flex-col justify:start gap-12">
+        {!!src && (
+          <Image
+            src={src}
+            width={450}
+            height={262}
+            alt="Card Image"
+            className="rounded-6 w-full md:w-90 xl:w-full h-auto"
+          />
+        )}
+        <div className="w-full flex flex-col gap-10">
+          <span className="text-black font-medium">{titleShort}</span>
+          <div className="flex flex-col md:flex-row xl:flex-col gap-10">
+            <div className="flex flex-row justify-start gap-6">
+              <Image src="assets/card/exampleChip/large1.svg" width={44} height={22} alt="chip" className="w-auto" />
+              <Image src="assets/card/exampleChip/large3.svg" width={44} height={22} alt="chip" className="w-auto" />
+            </div>
+            <div className="flex flex-row justify-between content-center w-full">
+              <div className="flex flex-row gap-6">
+                <Image src="assets/icon/calendar.svg" width={18} height={18} alt="date" className="inline-block" />
+                <span className="font-medium text-gray-78">{date}</span>
               </div>
-              <div className="flex flex-row justify-between content-center w-full">
-                <div className="flex flex-row gap-6">
-                  <Image src="assets/icon/calendar.svg" width={18} height={18} alt="date" className="inline-block" />
-                  <span className="font-medium text-gray-78">{date}</span>
-                </div>
-                <Image src="assets/chip/ellipseDefault.svg" width={24} height={24} alt="profileImg" />
-              </div>
+              <Image src={profile} width={24} height={24} alt="profileImg" />
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
