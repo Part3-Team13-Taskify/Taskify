2ba347d431c5c3293f710c5c2c999f3846b120ba
byunjiin <jibyun5808@naver.com>
2024-04-17 00:03:18 +0900

    ✨ Feat : useForm 활용해서 passwordInput 구현

--- src/components/common/input/exampleInput.tsx
+++ src/components/common/input/exampleInput.tsx
@@ -1,4 +1,5 @@
 import EmailInput from '@/src/components/common/input/emailInput';
+import PasswordInput from '@/src/components/common/input/passwordInput';
 import { FieldError, useForm } from 'react-hook-form';
 
 interface InputForm {
@@ -22,11 +23,11 @@ const ExampleInput = () => {
           register={register('email', {
             required: {
               value: true,
-              message: '이메일을 입력해 주세요.',
+              message: '이메일 형식으로 작성해 주세요.',
             },
             pattern: {
               value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-              message: '올바른 이메일 주소가 아닙니다.',
+              message: '이메일 형식으로 작성해 주세요.',
             },
           })}
           type="email"
@@ -35,6 +36,26 @@ const ExampleInput = () => {
           inputName="email"
           inputContent="이메일을 입력해 주세요"
           labelId="email"
+        />
+      </form>
+      <form className="flex flex-col items-start">
+        <PasswordInput
+          register={register('password', {
+            required: {
+              value: true,
+              message: '8자 이상 입력해 주세요.',
+            },
+            pattern: {
+              value: /^.{8,}$/,
+              message: '8자 이상 입력해 주세요.',
+            },
+          })}
+          type="password"
+          clearError={clearErrors}
+          error={errors.password as FieldError}
+          inputName="password"
+          inputContent="비밀번호를 입력해 주세요"
+          labelId="password"
         />
       </form>
     </>
---
+++ src/components/common/input/passwordInput/index.tsx
@@ -0,0 +1,57 @@
+import Image from 'next/image';
+import eyeOff from '@/public/assets/icon/eyeOff.svg';
+import eyeOn from '@/public/assets/icon/eyeOn.svg';
+import { FieldError, UseFormClearErrors, UseFormRegisterReturn } from 'react-hook-form';
+import { useState } from 'react';
+
+interface InputForm {
+  email: string;
+  password: string;
+}
+interface InputProps {
+  inputName: string;
+  inputContent: string;
+  labelId: string;
+  type: 'email' | 'password';
+  register: UseFormRegisterReturn;
+  error: FieldError;
+  clearError: UseFormClearErrors<InputForm>;
+}
+
+const PasswordInput = ({ register, inputName, inputContent, labelId, error, type, clearError }: InputProps) => {
+  const [openEye, setOpenEye] = useState(false);
+
+  const toggleEye = () => {
+    setOpenEye(!openEye);
+  };
+
+  return (
+    <>
+      <div className="flex flex-col items-start gap-8 text-gray-9f text-16">
+        <label className="text-black-33" htmlFor="password">
+          비밀번호
+        </label>
+        <div
+          className={
+            error?.message
+              ? 'flex flex-row justify-between w-520 h-50 py-15 px-16 border-1 rounded-lg border-red'
+              : 'flex flex-row  justify-between w-520 h-50 py-15 px-16 border-1 rounded-lg border-gray-9f focus:outline-none  focus:border-violet'
+          }
+        >
+          <input
+            {...register}
+            type={openEye ? 'text' : 'password'}
+            name={inputName}
+            className={error?.message ? ' text-black-33' : ' text-black-33 '}
+            placeholder={inputContent}
+            id={labelId}
+            onFocus={() => clearError('password')}
+          />
+          <Image className="" onClick={toggleEye} src={openEye ? eyeOn : eyeOff} alt={openEye ? 'eyeOn' : 'eyeOff'} />
+        </div>
+        {error?.message && <div className={'text-red text-14'}>{error.message}</div>}
+      </div>
+    </>
+  );
+};
+export default PasswordInput;
