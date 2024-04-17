6d3c0a9ec6843bb35a67de9e4553b9cae5583157
Harry K <155596644+harrykotter@users.noreply.github.com>
2024-04-17 22:12:14 +0900

    🤡 댓글, 할일 카드 mock-data 추가
    mock-data 추가 및 데이터를 통한 컴포넌트 재조정

--- src/components/Modal/TaskCard.tsx
+++ src/components/Modal/TaskCard.tsx
@@ -1,16 +1,17 @@
 import Image from 'next/image';
 import Reply from '../Reply';
 
+import taskData from '@/public/mock/TaskCard.json';
+import comments from '@/public/mock/Comment.json';
+import { format } from 'date-fns';
+
 export const TaskCard = () => {
-  const nickname = '정만철';
-  const date = '2024-04-16T06:28:59.964Z';
-  const replyContent = '오늘안에 CCC 까지 만들 수 있을까요?';
-  const profile = 'assets/chip/ellipseBlueLarge.svg';
+  const dueDate = format(new Date(taskData.dueDate), 'yyyy.MM.dd');
 
   return (
     <div className="px-28 py-32 w-full flex-shrink-0">
       <div className="grid text-24 font-bold grid-cols-2 grid-rows-2 sm:grid-rows-none justify-between">
-        <div className="row-start-2 col-start-1 sm:row-start-1">새로운 일정 Taskify</div>
+        <div className="row-start-2 col-start-1 sm:row-start-1">{taskData.title}</div>
         <div className="flex justify-end gap-24 row-start-1 col-start-2">
           <button>
             <Image src="assets/icon/moreVert.svg" width={28} height={28} alt="more" />
@@ -35,11 +36,7 @@ export const TaskCard = () => {
               <Image src="assets/card/exampleChip/large3.svg" width={44} height={22} alt="chip" className="w-auto" />
             </div>
           </div>
-          <p className="text-14 font-normal ">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum finibus nibh arcu, quis consequat ante
-            cursus eget. Cras mattis, nulla non laoreet porttitor, diam justo laoreet eros, vel aliquet diam elit at
-            leo.
-          </p>
+          <p className="text-14 font-normal ">{taskData.description}</p>
           <Image src="assets/card/desktop/card_image1.svg" width={450} height={263} alt="Task Image" />
           <div className="gap-24">
             <div className="flex flex-col">
@@ -54,22 +51,32 @@ export const TaskCard = () => {
               ></input>
             </div>
           </div>
-          <Reply nickname={nickname} profile={profile} date={date} content={replyContent} />
+          <div className="flex flex-col gap-20">
+            {comments.comments.map((comment) => (
+              <Reply
+                key={comment.id}
+                nickname={comment.author.nickname}
+                profile={comment.author.profileImageUrl}
+                date={comment.createdAt}
+                content={comment.content}
+              />
+            ))}
+          </div>
         </div>
         <div className="flex flex-col gap-6 border-1 border-gray-d9 rounded-8 w-200 max-h-160 p-16 min-w-180 my-16">
           <span className="text-12 font-semibold">담당자</span>
           <div className="flex flex-row justify-start content-center gap-8">
             <Image
-              src="assets/chip/ellipseBlueLarge.svg"
+              src={taskData.assignee.profileImageUrl}
               width={34}
               height={34}
               alt="profile"
               className="rounded-full"
             />
-            <span className="text-14 font-normal content-center">메뉴얼</span>
+            <span className="text-14 font-normal content-center">{taskData.assignee.nickname}</span>
           </div>
           <span className="text-12 font-semibold">마감일</span>
-          <span className="text-14 font-normal">2022.12.30 19:00</span>
+          <span className="text-14 font-normal">{dueDate}</span>
         </div>
       </div>
     </div>
