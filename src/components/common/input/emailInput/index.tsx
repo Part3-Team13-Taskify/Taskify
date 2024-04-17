d1c440bd15df65fc51c8467b89662b2c81fe290e
byunjiin <129635857+byunjiin@users.noreply.github.com>
2024-04-17 10:46:02 +0900

    ✨ Feat : input componts (#5)
    * ✨ Feat: 프로젝트 초기 설정
    
    * ✨ Feat : useForm 활용해서 emailInput 구현
    
    * ✨ Feat : useForm 활용해서 passwordInput 구현
    
    * ✨ Feat : useForm 활용해서 passwordInput 구현
    
    * Feat: 카드 공용 컴포넌트 구현 (#2)
    
    * ⬆️ Upgrade @types/react version
    
    기존 18 버전에서의 JSX 타입 미지정 이슈 해결
    
    * ✨ 카드 공통컴포넌트 예시 구현
    
    Tailwind css를 이용한 카드 예시 구현
    
    * 🎨 카드 컴포넌트 공용화
    
    props를 통한 카드 컴포넌트 커스텀화
    
    * 💄 Refactor: 카드 컴포넌트 반응형 구현
    
    화면 너비에 따른 반응형 구현 - md, xl 단위
    
    * 🎨 Refactor: 카드 컴포넌트 UI 최적화
    
    카드 컴포넌트 세부 단위 조정 및 날짜 입력 편의성을 고려한 date format 함수 추가
    
    * ✅ Refactor: 날짜 예시 조정
    
    날짜 예시를 ISO 8601 표준 형식으로 조정
    
    * ✨ Feat: landing page (#3)
    
    * ✨ Feat: landing page
    
    * ✨ Feat: landing page tablet mode
    
    * ✨ Feat: landing page mobile mode
    
    * ✨ Feat: landing page footer
    
    * 🐛 Fix: card paths
    
    * ✨ Feat: Header components (#4)
    
    ---------
    
    Co-authored-by: Harry K <155596644+harrykotter@users.noreply.github.com>
    Co-authored-by: Kihwan (Joel) Chong <joelkh784@gmail.com>
    Co-authored-by: 김경주 <114053155+gaeaeg@users.noreply.github.com>

---
+++ src/components/common/input/emailInput/index.tsx
@@ -0,0 +1,41 @@
+import { FieldError, UseFormClearErrors, UseFormRegisterReturn } from 'react-hook-form';
+
+interface InputForm {
+  email: string;
+  password: string;
+}
+interface InputProps {
+  inputName: string;
+  inputContent: string;
+  labelId: string;
+  type: 'email' | 'password';
+  register: UseFormRegisterReturn;
+  error: FieldError;
+  clearError: UseFormClearErrors<InputForm>;
+}
+
+const EmailInput = ({ register, inputName, inputContent, labelId, error, type, clearError }: InputProps) => {
+  return (
+    <div className="flex flex-col items-start gap-8 text-gray-9f text-16">
+      <label className="text-black-33" htmlFor="email">
+        이메일
+      </label>
+      <input
+        {...register}
+        type="email"
+        name={inputName}
+        className={
+          error?.message
+            ? 'w-520 h-50 py-15 px-16 border-1 rounded-lg border-red text-black-33'
+            : 'w-520 h-50 py-15 px-16 border-1 rounded-lg border-gray-9f text-black-33 focus:outline-none  focus:border-violet'
+        }
+        placeholder={inputContent}
+        id={labelId}
+        onFocus={() => clearError('email')}
+      />
+      {error?.message && <div className={'text-red text-14'}>{error.message}</div>}
+    </div>
+  );
+};
+
+export default EmailInput;
