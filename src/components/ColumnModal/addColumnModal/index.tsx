78ec032873e95a1fbeb8a2fabb3e08ef0a125b12
ysh73900 <74593476+ysh73900@users.noreply.github.com>
2024-04-18 20:11:06 +0900

    🚚 Rename: addColumnModal 파일 이동으로 인한 생성

---
+++ src/components/ColumnModal/addColumnModal/index.tsx
@@ -0,0 +1,48 @@
+import Button from '../../common/button';
+import Modal from '../../common/modal';
+
+interface AddColumnModalProps {
+  openModal: boolean;
+  handleModalClose: () => void;
+}
+
+const AddColumnModal: React.FC<AddColumnModalProps> = ({ openModal, handleModalClose }) => {
+  if (!openModal) {
+    return null;
+  }
+
+  return (
+    <Modal
+      className="w-540 mobile:w-327"
+      openModal={openModal}
+      handleModalClose={handleModalClose}
+      upperChildren={
+        <>
+          <div className="mb-32 text-24 font-bold">새 컬럼 생성</div>
+          <form className="flex flex-col">
+            <label className="mb-10 text-18 font-medium" htmlFor="columnName">
+              이름
+            </label>
+            <input
+              className="w-full h-48 px-16 mb-28 border-1 border-gray-d9 rounded-6"
+              type="text"
+              placeholder="새로운 프로젝트"
+            />
+          </form>
+        </>
+      }
+      lowerChildren={
+        <>
+          <Button buttonType="modal2" bgColor="violet" textColor="white">
+            생성
+          </Button>
+          <Button buttonType="modal2" bgColor="white" textColor="gray" onClick={handleModalClose}>
+            취소
+          </Button>
+        </>
+      }
+    />
+  );
+};
+
+export default AddColumnModal;
