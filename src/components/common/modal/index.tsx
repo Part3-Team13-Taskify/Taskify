f602eba37ec0dbcbf4dbdd9943ab1bf7dabeb3e9
ysh73900 <74593476+ysh73900@users.noreply.github.com>
2024-04-18 20:13:52 +0900

    ✨ Feat: twMerge 라이브러리 사용

--- src/components/common/modal/index.tsx
+++ src/components/common/modal/index.tsx
@@ -1,16 +1,29 @@
 import { ReactNode } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 interface ModalProps {
   upperChildren: ReactNode;
   lowerChildren: ReactNode;
   openModal: boolean;
-  // modalRef: RefObject<HTMLDivElement>;
   handleModalClose: () => void;
+  className?: string;
+  buttonClassName?: string;
 }
 
-const Modal: React.FC<ModalProps> = ({ upperChildren, lowerChildren, openModal, handleModalClose }) => {
-  const stylesModal =
-    'flex flex-col fixed top-1/2 left-1/2 w-548 bg-white rounded-8 zIndex-modal transform -translate-x-1/2 -translate-y-1/2 px-28 py-32 mobile:px-20 mobile:py-28';
+const Modal: React.FC<ModalProps> = ({
+  upperChildren,
+  lowerChildren,
+  openModal,
+  handleModalClose,
+  className,
+  buttonClassName,
+}) => {
+  const modalClass = twMerge(
+    'flex flex-col fixed top-1/2 left-1/2  bg-white rounded-8 zIndex-modal transform -translate-x-1/2 -translate-y-1/2 px-28 py-32  mobile:px-20 mobile:py-28',
+    className,
+  );
+
+  const buttonClass = twMerge('flex flex-row-reverse gap-12', buttonClassName);
 
   const open = 'block';
 
@@ -20,9 +33,9 @@ const Modal: React.FC<ModalProps> = ({ upperChildren, lowerChildren, openModal, 
   return (
     <>
       <div className="fixed left-0 top-0 w-full h-full bg-black-overlay zIndex" onClick={handleOverlayClick} />
-      <div className={`${stylesModal} ${openModal ? open : ''}`}>
+      <div className={`${modalClass} ${openModal ? open : ''}`}>
         {upperChildren}
-        <div className="flex flex-row-reverse gap-12">{lowerChildren}</div>
+        <div className={`${buttonClass}`}>{lowerChildren}</div>
       </div>
     </>
   );
