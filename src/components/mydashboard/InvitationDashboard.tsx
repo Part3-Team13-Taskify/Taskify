8096df37229b06979c736b0c731a281fcc402907
김경주 <popo505@naver.com>
2024-04-19 16:29:05 +0900

    🐛 Fix: mydashboard페이지 반응형 수정

--- src/components/mydashboard/InvitationDashboard.tsx
+++ src/components/mydashboard/InvitationDashboard.tsx
@@ -1,13 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchInvitations } from '@/src/pages/api/getInvitationApi';
 import Button from '@/src/components/common/button';
 import addLarge from '@/public/assets/chip/addLarge.svg';
+import unsubscribeEmail from '@/public/assets/icon/unsubscribeEmail.svg';
 import AddDashboardModal from '@/src/components/dashboardModal/addDashboardModal';
 import InvitationTable from '@/src/components/mydashboard/table/';
 import InvitationSearch from '@/src/components/mydashboard/table/InvitationSearch';
 import Image from 'next/image';
 
+interface Inviter {
+  nickname: string;
+  email: string;
+  id: number;
+}
+
+interface Dashboard {
+  title: string;
+  id: number;
+}
+
+interface Invitation {
+  id: number;
+  inviter: Inviter;
+  dashboard: Dashboard;
+}
+
 const InvitationDashboard = () => {
   const [isAddDashboardModalVisible, setIsAddDashboardModalVisible] = useState(false);
+  const [invitations, setInvitations] = useState<Invitation[]>([]);
 
   const showAddDashboardModal = () => {
     setIsAddDashboardModalVisible(true);
@@ -16,6 +36,19 @@ const InvitationDashboard = () => {
   const hideAddDashboardModal = () => {
     setIsAddDashboardModalVisible(false);
   };
+
+  useEffect(() => {
+    const fetchAndSetInvitations = async () => {
+      try {
+        const response = await fetchInvitations(10);
+        setInvitations(response.invitations);
+      } catch (error) {
+        console.error('초대 목록을 불러오는 데 실패했습니다.', error);
+      }
+    };
+
+    fetchAndSetInvitations();
+  }, []);
 
   return (
     <div className="flex flex-col">
@@ -36,14 +69,17 @@ const InvitationDashboard = () => {
       )}
       <div className="w-1022 h-auto mx-40 my-40 py-32 rounded-lg bg-white tablet:w-screen tablet:h-400 mobile:mx-24 mobile:h-screen">
         <h1 className="self-start pl-28 text-24 font-bold mobile:text-20">초대받은 대시보드</h1>
-        {/* <div className="flex flex-col items-center mt-66">
-          <Image src={unsubscribeEmail} alt="unsubscribeEmail " className="mb-24" />
-          <p className="text-18 font-normal text-gray-9f mobile:text-14">아직 초대받은 대시보드가 없어요</p>
-        </div> */}
-        <InvitationSearch />
-        <div className="">
-          <InvitationTable />
-        </div>
+        {invitations.length > 0 ? (
+          <>
+            <InvitationSearch />
+            <InvitationTable invitations={invitations} setInvitations={setInvitations} />
+          </>
+        ) : (
+          <div className="flex flex-col items-center mt-66">
+            <Image src={unsubscribeEmail} alt="unsubscribeEmail " className="mb-24" />
+            <p className="text-18 font-normal text-gray-9f mobile:text-14">아직 초대받은 대시보드가 없어요</p>
+          </div>
+        )}
       </div>
     </div>
   );
