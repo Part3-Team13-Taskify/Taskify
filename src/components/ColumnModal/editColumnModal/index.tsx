78151b65e22e2fa366a0abc51aeaa69ead36bc13
ysh73900 <74593476+ysh73900@users.noreply.github.com>
2024-04-22 19:44:10 +0900

    ✨Feat: columns 생성 기능 구현 (#33)
    * 📦️ Chore: esLint rule 추가
    
    * ✨ Feat: postColumns API 함수 구현
    
    * ✨ Feat: columns 생성 기능 구현
    
    * ♻️ Refactor: Modal refactor
    
    * ✨ Feat: 대쉬보드 페이지 버튼에 모달기능 추가
    
    * ✨ Feat: 토큰 기반 인증을 갖춘 axios 인스턴스 구현

--- src/components/ColumnModal/editColumnModal/index.tsx
+++ src/components/ColumnModal/editColumnModal/index.tsx
@@ -21,28 +21,19 @@ const EditColumnModal: React.FC<EditColumnModalProps> = ({ openModal, handleModa
   }
 
   return (
-    <Modal
-      className="w-540 mobile:w-327"
-      buttonClassName="justify-between mobile:flex-col-reverse mobile:gap-16"
-      openModal={openModal}
-      handleModalClose={handleModalClose}
-      upperChildren={
-        <>
-          <div className="mb-32 text-24 font-bold">컬럼 관리</div>
-          <form className="flex flex-col">
-            <label className="mb-10 text-18 font-medium" htmlFor="columnName">
-              이름
-            </label>
-            <input
-              className="w-full h-48 px-16 mb-28 border-1 border-gray-d9 rounded-6 mobile:mb-24"
-              type="text"
-              placeholder="선택한 컬럼 이름"
-            />
-          </form>
-        </>
-      }
-      lowerChildren={
-        <>
+    <Modal className="w-540 mobile:w-327" openModal={openModal} handleModalClose={handleModalClose}>
+      <div className="mb-32 text-24 font-bold">컬럼 관리</div>
+      <form className="flex flex-col">
+        <label className="mb-10 text-18 font-medium" htmlFor="columnName">
+          이름
+        </label>
+        <input
+          className="w-full h-48 px-16 mb-28 border-1 border-gray-d9 rounded-6 mobile:mb-24"
+          type="text"
+          placeholder="선택한 컬럼 이름"
+        />
+
+        <div className="flex flex-row-reverse justify-between mobile:flex-col-reverse">
           <div className="flex flex-row-reverse gap-12 mobile:gap-11 ">
             <Button buttonType="modal2" bgColor="violet" textColor="white">
               변경
@@ -56,13 +47,16 @@ const EditColumnModal: React.FC<EditColumnModalProps> = ({ openModal, handleModa
             <ModalPortal>
               <DeleteColumnModal openModal={deleteColumnModal} handleModalClose={deleteColumnModalClose} />
             </ModalPortal>
-            <span className="text-14 text-gray-78 underline cursor-pointer" onClick={deleteColumnModalOpen}>
+            <span
+              className="text-14 text-gray-78 underline cursor-pointer mobile:mb-16"
+              onClick={deleteColumnModalOpen}
+            >
               삭제하기
             </span>
           </div>
-        </>
-      }
-    />
+        </div>
+      </form>
+    </Modal>
   );
 };
 
--- src/components/common/modal/index.tsx
+++ src/components/common/modal/index.tsx
@@ -2,28 +2,18 @@ import { ReactNode } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 interface ModalProps {
-  upperChildren: ReactNode;
-  lowerChildren: ReactNode;
+  children: ReactNode;
   openModal: boolean;
   handleModalClose: () => void;
   className?: string;
   buttonClassName?: string;
 }
 
-const Modal: React.FC<ModalProps> = ({
-  upperChildren,
-  lowerChildren,
-  openModal,
-  handleModalClose,
-  className,
-  buttonClassName,
-}) => {
+const Modal: React.FC<ModalProps> = ({ children, openModal, handleModalClose, className }) => {
   const modalClass = twMerge(
     'flex flex-col fixed top-1/2 left-1/2  bg-white rounded-8 zIndex-modal transform -translate-x-1/2 -translate-y-1/2 px-28 py-32  mobile:px-20 mobile:py-28',
     className,
   );
-
-  const buttonClass = twMerge('flex flex-row-reverse gap-12', buttonClassName);
 
   const open = 'block';
 
@@ -33,10 +23,7 @@ const Modal: React.FC<ModalProps> = ({
   return (
     <>
       <div className="fixed left-0 top-0 w-full h-full bg-black-overlay zIndex" onClick={handleOverlayClick} />
-      <div className={`${modalClass} ${openModal ? open : ''}`}>
-        {upperChildren}
-        <div className={`${buttonClass}`}>{lowerChildren}</div>
-      </div>
+      <div className={`${modalClass} ${openModal ? open : ''}`}>{children}</div>
     </>
   );
 };
