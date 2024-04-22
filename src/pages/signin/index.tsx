b4c0bb55b04382b90ffcedae8172ca5a19103d7a
byunjiin <jibyun5808@naver.com>
2024-04-23 02:03:36 +0900

    🐛 Refator: Sign페이지들 바뀐 Input태그에 맞게 수정

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -4,6 +4,7 @@ import PasswordInput from '@/src/components/common/input/passwordInput';
 import Button from '@/src/components/common/button';
 import { FieldError, useForm } from 'react-hook-form';
 import Input from '@/src/components/common/input';
+import Link from 'next/link';
 
 interface InputForm {
   text?: string;
@@ -11,21 +12,25 @@ interface InputForm {
   password: string;
   newpassword?: string;
   passwordcheck?: string;
+  checkbox?: boolean;
   file?: string;
 }
 
 const Signin = () => {
   const {
     register,
-    handleSubmit, // handleSubmit 추가
+    handleSubmit,
     formState: { errors },
     clearErrors,
+    getValues,
   } = useForm<InputForm>({ mode: 'onBlur', reValidateMode: 'onBlur' });
 
   const onSubmit = (data: InputForm) => {
-    // 폼 제출 시 실행되는 함수
-    console.log(data); // 입력된 데이터 확인용
+    console.log(data);
   };
+
+  const newEmailValue = getValues('email');
+  console.log(newEmailValue);
 
   return (
     <div>
@@ -35,10 +40,8 @@ const Signin = () => {
           <h1 className="text-20 mb-6 font-medium">오늘도 만나서 반가워요!</h1>
         </header>
         <main className="w-520 mobile:w-351">
-          <div className="pb-16">
-            <form onSubmit={handleSubmit(onSubmit)}>
-              {' '}
-              {/* handleSubmit 함수를 onSubmit 이벤트 핸들러로 설정 */}
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="pb-16">
               <Input
                 register={register('email', {
                   required: {
@@ -59,9 +62,6 @@ const Signin = () => {
                 labelText="이메일"
                 focusType="email"
               />
-            </form>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              {/* handleSubmit 함수를 onSubmit 이벤트 핸들러로 설정 */}
               <PasswordInput
                 register={register('password', {
                   required: {
@@ -79,25 +79,26 @@ const Signin = () => {
                 inputName="password"
                 inputContent="비밀번호를 입력해 주세요"
                 labelId="password"
-                labelName="비밀번호"
+                labelText="비밀번호"
               />
-            </form>
-          </div>
-          <Button
-            type="submit"
-            buttonType="login"
-            bgColor="violet"
-            textColor="white"
-            disabled={Object.keys(errors).length !== 0} // errors 객체가 비어있지 않으면 disabled로 설정
-          >
-            로그인
-          </Button>
+            </div>
+            <Button
+              type="submit"
+              buttonType="login"
+              bgColor="violet"
+              textColor="white"
+              disabled={Object.keys(errors).length !== 0}
+            >
+              로그인
+            </Button>
+          </form>
         </main>
         <footer>
           <p>
             회원이 아니신가요?
-            {/* prettier-ignore */}
-            <a className="underline text-violet" href="/signup">  회원가입하기</a>
+            <Link className="underline text-violet pl-10" href="/signup">
+              회원가입하기
+            </Link>
           </p>
         </footer>
       </div>
