1f91a2673c7e103ad4c0aabcd9bf3ae3a05f778d
Harry K <155596644+harrykotter@users.noreply.github.com>
2024-04-22 10:21:07 +0900

    ✨ Axios instance를 통한 api fetch 구현
    axios를 통한 api fetch, 토큰 첨부, 베이스 URL 입력

---
+++ src/util/axiosInstance.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+    baseURL: 'https://sp-taskify-api.vercel.app/4-13/',
+})
+
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTk5MywidGVhbUlkIjoiNC0xMyIsImlhdCI6MTcxMzcxMzk0NiwiaXNzIjoic3AtdGFza2lmeSJ9.r2xKX_D4sv3R77JjPt--Wm9dGycyzusV6s9nnXtYz74'
+
+axiosInstance.interceptors.request.use((config)=>{
+    config.headers.Authorization = `Bearer ${token}`
+    return config
+})
