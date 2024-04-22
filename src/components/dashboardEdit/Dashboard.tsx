c024fd76bfd3bacad7c9b7ece60dccbb6d2fc0b4
Kihwan (Joel) Chong <joelkh784@gmail.com>
2024-04-22 22:38:22 +0900

    ✨Feat: dashboard edit api (#34)
    * ✨ Feat: api for invitation
    
    * ✨ Feat: edit dashboard title
    
    * ✨ Feat: dashboard edit color picker api

--- src/components/dashboardEdit/Dashboard.tsx
+++ src/components/dashboardEdit/Dashboard.tsx
@@ -1,70 +1,188 @@
 import Image from 'next/image';
 import add from '@/public/assets/icon/addWhite.svg';
-import ellipse from '@/public/assets/chip/ellipseGreenLarge.svg';
+import instance from '@/src/util/axios';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { useForm } from 'react-hook-form';
 import ColorPicker from '../common/colorpicker';
-// import ExampleInput from '../common/input/exampleInput';
 import Button from '../common/button';
 import Table from './table';
 import TableHeader from './table/TableHeader';
 import TableList from './table/TableList';
 import DashboardCard from './DashboardCard';
+import Input, { InputForm } from '../common/input';
+
+type DashboardData = {
+  title: string;
+  color: string;
+  createdByMe: boolean;
+};
+
+type Members = {
+  id: number;
+  email: string;
+  nickname: string;
+  profileImageUrl: string;
+  isOwner: boolean;
+  userId: number;
+};
+
+type Invitees = {
+  email: string;
+};
 
 const Dashboard = () => {
+  const [dashboardData, setDashboardData] = useState<DashboardData>({ title: '', color: '', createdByMe: false });
+  const [members, setMembers] = useState<Members>({
+    id: 0,
+    email: '',
+    nickname: '',
+    profileImageUrl: '',
+    isOwner: false,
+    userId: 0,
+  });
+  const [invitees, setInvitees] = useState<Invitees>({ email: '' });
+  const { register, getValues, handleSubmit } = useForm<InputForm>({ mode: 'onBlur', reValidateMode: 'onBlur' });
+  const [selectedColor, setSelectedColor] = useState<string>(dashboardData.color);
+
+  const router = useRouter();
+  const { id } = router.query;
+  console.log(id);
+  const getDashboard = async () => {
+    try {
+      // const response = await instance.get('/dashboards?navigationMethod=pagination&page=1&size=10');
+      const response = await instance.get(`/dashboards/${id}`);
+
+      console.log(response.data);
+      setDashboardData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getMembers = async () => {
+    try {
+      // const response = await instance.get('/dashboards?navigationMethod=pagination&page=1&size=10');
+      const response = await instance.get(`/members?page=1&size=20&dashboardId=${id}`);
+
+      console.log(response.data.members[0]);
+      setMembers(response.data.members);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getInvitees = async () => {
+    try {
+      // const response = await instance.get('/dashboards?navigationMethod=pagination&page=1&size=10');
+      const response = await instance.get(`/dashboards/${id}/invitations?page=1&size=10`);
+
+      console.log(response.data.invitations[0]);
+      setInvitees(response.data.invitations);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getDashboard();
+    getMembers();
+    getInvitees();
+  }, [id]);
+
+  const handleDeleteDashboard = async () => {
+    try {
+      await instance.delete(`/dashboards/${id}`);
+      router.push('/my-dashboard');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleDeleteMember = async ({ userId }: { userId: number }) => {
+    try {
+      await instance.delete(`/members/${userId}`);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleCancelInvitation = async ({ invitationId }: { invitationId: number }) => {
+    try {
+      await instance.delete(`/dashboards/${id}/invitations/${invitationId}`);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleEditDashboard = async () => {
+    const dashboardTitle = getValues('text') || '';
+    try {
+      const data = { title: dashboardTitle, color: selectedColor };
+      await instance.put(`/dashboards/${id}`, data);
+      getDashboard();
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div className="flex flex-col gap-25 tablet:gap-12">
       <DashboardCard>
         <div className="flex justify-between">
-          <p className="font-bold text-20">비브리지</p>
-          <ColorPicker />
+          <p className="font-bold text-20">{dashboardData.title}</p>
+          {/* <ColorPicker color={dashboardData.color} /> */}
+          <ColorPicker selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
         </div>
-        {/* 인풋 컴포넌트로 대체 */}
-        {/* <ExampleInput /> */}
-        <div className="flex justify-end">
-          <Button buttonType="modal1" bgColor="violet" textColor="white">
-            변경
-          </Button>
-        </div>
+        <form onSubmit={handleSubmit(handleEditDashboard)}>
+          <Input
+            inputName="text"
+            inputContent={dashboardData.title}
+            labelId="text"
+            labelText="대시보드 이름"
+            type="text"
+            register={register('text', {
+              required: {
+                value: true,
+                message: '대시보드 이름을 입력해주세요',
+              },
+            })}
+            inputCheckStyle="flex my-10"
+            labelDropStyle="w-full"
+          />
+          <div className="flex justify-end">
+            <Button
+              buttonType="modal1"
+              type="submit"
+              bgColor="violet"
+              textColor="white"
+              onClick={handleSubmit(handleEditDashboard)}
+            >
+              변경
+            </Button>
+          </div>
+        </form>
       </DashboardCard>
 
       <DashboardCard className="px-0 pb-0">
         <TableHeader title="구성원" />
         <Table label="이름">
-          <TableList
-            src={ellipse}
-            text="정만철"
-            button={
-              <Button buttonType="delete" textColor="violet" bgColor="white">
-                삭제
-              </Button>
-            }
-          />
-          <TableList
-            src={ellipse}
-            text="정만철"
-            button={
-              <Button buttonType="delete" textColor="violet" bgColor="white">
-                삭제
-              </Button>
-            }
-          />
-          <TableList
-            src={ellipse}
-            text="정만철"
-            button={
-              <Button buttonType="delete" textColor="violet" bgColor="white">
-                삭제
-              </Button>
-            }
-          />
-          <TableList
-            src={ellipse}
-            text="정만철"
-            button={
-              <Button buttonType="delete" textColor="violet" bgColor="white">
-                삭제
-              </Button>
-            }
-          />
+          {Array.isArray(members) &&
+            members.map((member) => (
+              <TableList
+                src={member.profileImageUrl}
+                text={member.nickname}
+                button={
+                  <Button
+                    buttonType="delete"
+                    textColor="violet"
+                    bgColor="white"
+                    onClick={() => handleDeleteMember(member.userId)}
+                  >
+                    삭제
+                  </Button>
+                }
+              />
+            ))}
         </Table>
       </DashboardCard>
 
@@ -83,41 +201,25 @@ const Dashboard = () => {
           </Button>
         </TableHeader>
         <Table label="이메일">
-          <TableList
-            text="codeitA@codeit.com"
-            button={
-              <Button buttonType="delete" textColor="violet" bgColor="white">
-                취소
-              </Button>
-            }
-          />
-          <TableList
-            text="codeitA@codeit.com"
-            button={
-              <Button buttonType="delete" textColor="violet" bgColor="white">
-                취소
-              </Button>
-            }
-          />
-          <TableList
-            text="codeitA@codeit.com"
-            button={
-              <Button buttonType="delete" textColor="violet" bgColor="white">
-                취소
-              </Button>
-            }
-          />
-          <TableList
-            text="codeitA@codeit.com"
-            button={
-              <Button buttonType="delete" textColor="violet" bgColor="white">
-                취소
-              </Button>
-            }
-          />
+          {Array.isArray(invitees) &&
+            invitees.map((invitee) => (
+              <TableList
+                text={invitee.invitee.email}
+                button={
+                  <Button
+                    buttonType="delete"
+                    textColor="violet"
+                    bgColor="white"
+                    onClick={() => handleCancelInvitation(invitee.dashboard.id)}
+                  >
+                    취소
+                  </Button>
+                }
+              />
+            ))}
         </Table>
       </DashboardCard>
-      <Button buttonType="dashboardDelete" bgColor="white" className="mt-25">
+      <Button buttonType="dashboardDelete" bgColor="white" className="mt-25" onClick={handleDeleteDashboard}>
         대시보드 삭제하기
       </Button>
     </div>
--- src/util/axios.ts
+++ src/util/axios.ts
@@ -1,15 +1,16 @@
 import axios from 'axios';
 
-const axiosInstance = axios.create({
+const instance = axios.create({
   baseURL: 'https://sp-taskify-api.vercel.app/4-13/',
 });
 
 const token =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTk5MywidGVhbUlkIjoiNC0xMyIsImlhdCI6MTcxMzcxMzk0NiwiaXNzIjoic3AtdGFza2lmeSJ9.r2xKX_D4sv3R77JjPt--Wm9dGycyzusV6s9nnXtYz74';
 
-axiosInstance.interceptors.request.use((config) => {
-  config.headers.Authorization = `Bearer ${token}`;
-  return config;
+instance.interceptors.request.use((config) => {
+  const modifiedConfig = { ...config };
+  modifiedConfig.headers.Authorization = `Bearer ${token}`;
+  return modifiedConfig;
 });
 
-export default axiosInstance;
+export default instance;
