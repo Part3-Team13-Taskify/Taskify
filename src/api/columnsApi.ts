8f4ce80a3859eb1008b419bdcb57b8a60a3ed4fc
ysh73900 <74593476+ysh73900@users.noreply.github.com>
2024-04-22 19:33:47 +0900

    ✨ Feat: postColumns API 함수 구현

---
+++ src/api/columnsApi.ts
@@ -0,0 +1,23 @@
+import axios from '../util/axios';
+
+interface ColumnData {
+  title: string;
+  dashboardId: number | undefined;
+}
+
+export const postColumns = async (columnData: ColumnData) => {
+  try {
+    const response = await axios.post('columns', columnData);
+    console.log('Response data:', response.data);
+    // return response.data;
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error('Error posting Columns data:', error.message);
+    } else {
+      console.error('An unexpected error occurred:', error);
+    }
+    throw error;
+  }
+};
+
+export const getColumns = () => {};
