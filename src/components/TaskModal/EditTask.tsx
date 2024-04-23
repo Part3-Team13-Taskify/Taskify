7be36e192d3ac5bd0745c068ddd3fcb2714d7efd
Harry K <155596644+harrykotter@users.noreply.github.com>
2024-04-23 16:05:36 +0900

    ♻️Refactor: TaskCard 컴포넌트 api 연결 구현  (#37)
    * ✨ Axios instance를 통한 api fetch 구현
    
    axios를 통한 api fetch, 토큰 첨부, 베이스 URL 입력
    
    * 🎨 Modal 컴포넌트 변경에 따른 크도 수정
    
    공용 Modal 컴포넌트 수정에 따른 할일 생성, 할일 카드, 할일 수정 모달 조정
    
    * 🎨 TaskCard 컴포넌트 api 연결 구현
    
    axios instance 구조 개선 및 할일카드 컴포넌트 api 연결 구현
    
    * 🎨 Chip 공통 컴포넌트 적용
    
    현재 상태에 따른 Chip 공통 컴포넌트 적용

--- src/components/TaskModal/EditTask.tsx
+++ src/components/TaskModal/EditTask.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import TaskLabel from '../TaskLabel';
+import TaskLabel from './TaskLabel';
 import Button from '../common/button';
 import { ChangeEventHandler, useState } from 'react';
 import Modal from '../common/modal';
@@ -35,9 +35,9 @@ const EditTask: React.FC<EditTaskModalProps> = ({ openModal, handleModalClose })
 
   const [titleValue, setTitleValue] = useState<string>(TASK_DATA.title);
   const [desrciptionValue, setDesrciptionValue] = useState<string>(TASK_DATA.description);
-  const [status, setStatus] = useState<string>('')
-  const [due, setDue] = useState<string>(TASK_DATA.dueDate)
-  const [tag, setTag] = useState<string>('')
+  const [status, setStatus] = useState<string>('');
+  const [due, setDue] = useState<string>(TASK_DATA.dueDate);
+  const [tag, setTag] = useState<string>('');
   const isRequiredFilled = desrciptionValue && titleValue;
 
   const handleTitleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
@@ -46,112 +46,103 @@ const EditTask: React.FC<EditTaskModalProps> = ({ openModal, handleModalClose })
   const handleDescriptionChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
     setDesrciptionValue(e.target.value.trim());
   };
-  const handleDateChange: ChangeEventHandler<HTMLInputElement> =(e) =>{
+  const handleDateChange: ChangeEventHandler<HTMLInputElement> = (e) => {
     setDue(e.target.value.trim());
-  }
-  const handleStatusChange: ChangeEventHandler<HTMLSelectElement> =(e)=>{
+  };
+  const handleStatusChange: ChangeEventHandler<HTMLSelectElement> = (e) => {
     setStatus(e.target.value);
-  }
-  const handleTagChange: ChangeEventHandler<HTMLInputElement> =(e)=>{
-    setTag(e.target.value.trim())
-  }
+  };
+  const handleTagChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setTag(e.target.value.trim());
+  };
 
   return (
-    <Modal
-      className="max-w-540 w-full max-h-910 h-svh"
-      openModal={openModal}
-      handleModalClose={handleModalClose}
-      upperChildren={
-        <>
-          <div className="text-24 font-bold">할 일 생성</div>
-          <form className="flex flex-col gap-32 overflow-y-auto">
-            <div className='flex flex-row mobile:flex-col gap-16 mobile:gap-24'>
-            <TaskLabel htmlFor="status" label="상태">
-              <select
-                id="status"
-                value={status}
-                onSelect={handleStatusChange}
-                className="max-w-217 w-full border-1 border-gray-9f rounded-6 focus:border-violet p-15 mobile:max-w-none"
-              >
-                <option value="to-do">To Do</option>
-                <option value="done">Done</option>
-                <option value="on-progress">On Progress</option>
-              </select>
-            </TaskLabel>
-            <TaskLabel htmlFor="assignee" label="담당자">
-              <select
-                id="assignee"
-                className="max-w-217 w-full border-1 border-gray-9f rounded-6 focus:border-violet p-15 mobile:max-w-none"
-              >
-                <option value="" className="text-gray">
-                  이름을 입력해 주세요
-                </option>
-                <option value="1">{TASK_DATA.assignee[0]}</option>
-                <option value="2">{TASK_DATA.assignee[1]}</option>
-                <option value="3">{TASK_DATA.assignee[2]}</option>
-              </select>
-            </TaskLabel>
-            
-            </div>
-            <TaskLabel htmlFor="title" label="제목" isRequired={true}>
-              <input
-                id="title"
-                className="border-1 border-gray-9f rounded-6 focus:border-violet p-15"
-                placeholder="제목을 입력해 주세요"
-                value={titleValue}
-                required
-                onChange={handleTitleChange}
-              ></input>
-            </TaskLabel>
-            <TaskLabel htmlFor="description" label="설명" isRequired={true}>
-              <textarea
-                id="description"
-                className="resize-none border-1 border-gray-9f rounded-6 focus-within:border-violet p-15"
-                placeholder="설명을 입력해 주세요"
-                required
-                value={desrciptionValue}
-                onChange={handleDescriptionChange}
-              ></textarea>
-            </TaskLabel>
-            <TaskLabel htmlFor="due-date" label="마감일">
-              <input
-                id="due-date"
-                type="text"
-                placeholder="날짜를 입력해 주세요"
-                value={due}
-                onChange={handleDateChange}
-                className="border-1 border-gray-9f rounded-6 focus-within:border-violet p-15"
-              ></input>
-            </TaskLabel>
-            <TaskLabel htmlFor="tag" label="태그">
-              <input
-                id="tag"
-                type="text"
-                placeholder="입력 후 Enter"
-                onChange={handleTagChange}
-                value={tag}
-                className="border-1 border-gray-9f rounded-6 focus-within:border-violet p-15"
-              ></input>
-            </TaskLabel>
-            <TaskLabel htmlFor="image" label="이미지">
-              <button className="p-24 bg-gray-9f w-fit rounded-6">
-                <Image src="assets/chip/addSmall.svg" width={28} height={28} alt="add image"></Image>
-              </button>
-            </TaskLabel>
-          </form>
-        </>
-      }
-      lowerChildren={
-        <>
-          <Button buttonType="modal2" bgColor="violet" textColor="white" disabled={!isRequiredFilled}>
-            수정
-          </Button>
-          <Button buttonType="modal2" bgColor="white" textColor="gray" onClick={handleModalClose}>
-            취소
-          </Button>
-        </>
-      }
-    ></Modal>
+    <Modal className="max-w-540 w-full max-h-910 h-svh" openModal={openModal} handleModalClose={handleModalClose}>
+      {' '}
+      <div className="text-24 font-bold">할 일 생성</div>
+      <form className="flex flex-col gap-32 overflow-y-auto">
+        <div className="flex flex-row mobile:flex-col gap-16 mobile:gap-24">
+          <TaskLabel htmlFor="status" label="상태">
+            <select
+              id="status"
+              value={status}
+              onSelect={handleStatusChange}
+              className="max-w-217 w-full border-1 border-gray-9f rounded-6 focus:border-violet p-15 mobile:max-w-none"
+            >
+              <option value="to-do">To Do</option>
+              <option value="done">Done</option>
+              <option value="on-progress">On Progress</option>
+            </select>
+          </TaskLabel>
+          <TaskLabel htmlFor="assignee" label="담당자">
+            <select
+              id="assignee"
+              className="max-w-217 w-full border-1 border-gray-9f rounded-6 focus:border-violet p-15 mobile:max-w-none"
+            >
+              <option value="" className="text-gray">
+                이름을 입력해 주세요
+              </option>
+              <option value="1">{TASK_DATA.assignee[0]}</option>
+              <option value="2">{TASK_DATA.assignee[1]}</option>
+              <option value="3">{TASK_DATA.assignee[2]}</option>
+            </select>
+          </TaskLabel>
+        </div>
+        <TaskLabel htmlFor="title" label="제목" isRequired={true}>
+          <input
+            id="title"
+            className="border-1 border-gray-9f rounded-6 focus:border-violet p-15"
+            placeholder="제목을 입력해 주세요"
+            value={titleValue}
+            required
+            onChange={handleTitleChange}
+          ></input>
+        </TaskLabel>
+        <TaskLabel htmlFor="description" label="설명" isRequired={true}>
+          <textarea
+            id="description"
+            className="resize-none border-1 border-gray-9f rounded-6 focus-within:border-violet p-15"
+            placeholder="설명을 입력해 주세요"
+            required
+            value={desrciptionValue}
+            onChange={handleDescriptionChange}
+          ></textarea>
+        </TaskLabel>
+        <TaskLabel htmlFor="due-date" label="마감일">
+          <input
+            id="due-date"
+            type="text"
+            placeholder="날짜를 입력해 주세요"
+            value={due}
+            onChange={handleDateChange}
+            className="border-1 border-gray-9f rounded-6 focus-within:border-violet p-15"
+          ></input>
+        </TaskLabel>
+        <TaskLabel htmlFor="tag" label="태그">
+          <input
+            id="tag"
+            type="text"
+            placeholder="입력 후 Enter"
+            onChange={handleTagChange}
+            value={tag}
+            className="border-1 border-gray-9f rounded-6 focus-within:border-violet p-15"
+          ></input>
+        </TaskLabel>
+        <TaskLabel htmlFor="image" label="이미지">
+          <button className="p-24 bg-gray-9f w-fit rounded-6">
+            <Image src="assets/chip/addSmall.svg" width={28} height={28} alt="add image"></Image>
+          </button>
+        </TaskLabel>
+      </form>
+      <div className="flex flex-row-reverse gap-12">
+        <Button buttonType="modal2" bgColor="violet" textColor="white" disabled={!isRequiredFilled}>
+          수정
+        </Button>
+        <Button buttonType="modal2" bgColor="white" textColor="gray" onClick={handleModalClose}>
+          취소
+        </Button>
+      </div>
+    </Modal>
   );
 };
 
--- src/components/TaskModal/TaskCard.tsx
+++ src/components/TaskModal/TaskCard.tsx
@@ -6,28 +6,70 @@ import comments from '@/public/mock/Comment.json';
 import { format } from 'date-fns';
 import { ChangeEventHandler, MouseEventHandler, useEffect, useRef, useState } from 'react';
 import Modal from '../common/modal';
+import instance from '@/src/util/axios';
+import Chip from '../common/chip';
 
-interface TaskModalProps {
+interface ModalProps {
   openModal: boolean;
   handleModalClose: () => void;
 }
 
-export const TaskCard = ({openModal, handleModalClose}:TaskModalProps) => {
+interface TaskModalProps extends ModalProps {
+  cardId: number;
+  columnName: string;
+}
+
+interface TaskData {
+  assignee: {
+    id: number;
+    nickname: string;
+    profileImageUrl?: string;
+  };
+  columnId: number;
+  createdAt: string;
+  dashboardId: number;
+  description: string;
+  dueDate?: string;
+  id: number;
+  imageUrl?: string;
+  tags?: string[];
+  teamId: string;
+  title: string;
+  updatedAt: string;
+}
+
+export const TaskCard = ({ openModal, handleModalClose, cardId, columnName }: TaskModalProps) => {
   if (!openModal) {
     return null;
   }
-  
+
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [replyValue, setReplyValue] = useState('');
+  const [cardData, setCardData] = useState<TaskData>();
+  const [isPending, setIsPending] = useState(true);
 
-  const dueDate = format(new Date(taskData.dueDate).toLocaleString('en-US'), 'yyyy.MM.dd');
+  useEffect(() => {
+    const getTaskData = async () => {
+      try {
+        const response = await instance.get(`cards/${cardId}`);
+        setCardData(response.data);
+      } catch {
+        console.log('error');
+      } finally {
+        setIsPending(false);
+      }
+    };
+    getTaskData();
+  }, []);
+
+  const dueDate = cardData?.dueDate ? format(new Date(cardData.dueDate).toLocaleString('en-US'), 'yyyy.MM.dd') : '';
   const dropdownRef = useRef<HTMLDivElement>(null);
 
   const handleDropdownClick: MouseEventHandler<HTMLButtonElement> = () => setIsDropdownOpen(true);
-
   const handleTextChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
     setReplyValue(e.target.value.trim());
   };
+
   useEffect(() => {
     const handleOutsideClick = (e: MouseEvent) => {
       if (dropdownRef.current && !dropdownRef.current.contains(e.target as Node)) {
@@ -38,98 +80,96 @@ export const TaskCard = ({openModal, handleModalClose}:TaskModalProps) => {
     return () => {
       document.removeEventListener('mousedown', handleOutsideClick);
     };
-  },[]);
+  }, []);
+  console.log(cardData);
 
-  return (
-    <Modal className='flex-shrink-0' openModal={openModal} handleModalClose={handleModalClose} upperChildren={
-    <>
-      <div className="grid grid-cols-2 grid-rows-2 sm:grid-rows-none justify-between">
-        <div className="row-start-2 col-start-1 sm:row-start-1 text-24 font-semibold">{taskData.title}</div>
-        <div className="flex justify-end gap-24 row-start-1 col-start-2">
-          {isDropdownOpen && (
-            <div
-              className="flex flex-col absolute border-1 rounded-6 p-6 text-14 font-normal bg-white top-65 right-95"
-              ref={dropdownRef}
-            >
-              <button className="px-16 py-4 rounded-6 hover:text-violet hover:bg-violet-8%">수정</button>
-              <button className="px-16 py-4 rounded-6 hover:text-violet hover:bg-violet-8%">삭제</button>
-            </div>
-          )}
-          <button onClick={handleDropdownClick}>
-            <Image src="assets/icon/moreVert.svg" width={28} height={28} alt="more" />
-          </button>
-          <button onClick={handleModalClose}>
-            <Image src="assets/icon/close.svg" width={28} height={28} alt="exit" />
-          </button>
-        </div>
-      </div>
-      <div className="flex flex-row gap-32">
-        <div className="flex flex-col gap-16 max-w-450 mt-12">
-          <div className="flex flex-row gap-12">
-            <Image
-              src="assets/card/exampleChip/chip/todoLarge.svg"
-              width={39}
-              height={12}
-              alt="todo"
-              className="w-auto"
-            />
-            <div className="flex flex-row gap-6 border-l-1 border-gray-d9 pl-12">
-              <Image src="assets/card/exampleChip/large1.svg" width={44} height={22} alt="chip" className="w-auto" />
-              <Image src="assets/card/exampleChip/large3.svg" width={44} height={22} alt="chip" className="w-auto" />
-            </div>
-          </div>
-          <p className="text-14 font-normal ">{taskData.description}</p>
-          <Image src="assets/card/desktop/card_image1.svg" width={450} height={263} alt="Task Image" />
-          <div className="gap-24">
-            <div className="flex flex-col relative">
-              <label htmlFor="reply" className="my-10">
-                댓글
-              </label>
-              <textarea
-                id="reply"
-                className="p-16 border-1 border-gray-d9 rounded-6 h-110 text-14 resize-none"
-                placeholder="댓글 작성하기"
-                onChange={handleTextChange}
-              ></textarea>
-              <button
-                className={`absolute bottom-12 right-12 border rounded-6 border-gray-df px-31 py-6 ${
-                  replyValue ? 'bg-white text-violet' : 'bg-gray-50 text-gray-78'
-                }`}
-                disabled={!replyValue}
+  if (!isPending)
+    return (
+      <Modal className="flex-shrink-0" openModal={openModal} handleModalClose={handleModalClose}>
+        <div className="grid grid-cols-2 grid-rows-2 sm:grid-rows-none justify-between">
+          <div className="row-start-2 col-start-1 sm:row-start-1 text-24 font-semibold">{cardData?.title}</div>
+          <div className="flex justify-end gap-24 row-start-1 col-start-2">
+            {isDropdownOpen && (
+              <div
+                className="flex flex-col absolute border-1 rounded-6 p-6 text-14 font-normal bg-white top-65 right-95"
+                ref={dropdownRef}
               >
-                입력
-              </button>
+                <button className="px-16 py-4 rounded-6 hover:text-violet hover:bg-violet-8%">수정</button>
+                <button className="px-16 py-4 rounded-6 hover:text-violet hover:bg-violet-8%">삭제</button>
+              </div>
+            )}
+            <button onClick={handleDropdownClick}>
+              <Image src="assets/icon/moreVert.svg" width={28} height={28} alt="more" />
+            </button>
+            <button onClick={handleModalClose}>
+              <Image src="assets/icon/close.svg" width={28} height={28} alt="exit" />
+            </button>
+          </div>
+        </div>
+        <div className="flex flex-row gap-32">
+          <div className="flex flex-col gap-16 max-w-450 mt-12">
+            <div className="flex flex-row gap-12">
+              <Chip dot={true}>{columnName}</Chip>
+              <div className="flex flex-row gap-6 border-l-1 border-gray-d9 pl-12">
+                <Chip color="orange">프로젝트</Chip>
+                <Chip color="pink">백엔드</Chip>
+              </div>
+            </div>
+            <p className="text-14 font-normal ">{cardData?.description}</p>
+            <Image src="assets/card/desktop/card_image1.svg" width={450} height={263} alt="Task Image" />
+            <div className="gap-24">
+              <div className="flex flex-col relative">
+                <label htmlFor="reply" className="my-10">
+                  댓글
+                </label>
+                <textarea
+                  id="reply"
+                  className="p-16 border-1 border-gray-d9 rounded-6 h-110 text-14 resize-none"
+                  placeholder="댓글 작성하기"
+                  onChange={handleTextChange}
+                ></textarea>
+                <button
+                  className={`absolute bottom-12 right-12 border rounded-6 border-gray-df px-31 py-6 ${
+                    replyValue ? 'bg-white text-violet' : 'bg-gray-50 text-gray-78'
+                  }`}
+                  disabled={!replyValue}
+                >
+                  입력
+                </button>
+              </div>
+            </div>
+            <div className="flex flex-col gap-20">
+              {comments.comments.map((comment) => (
+                <Reply
+                  key={comment.id}
+                  nickname={comment.author.nickname}
+                  profile={comment.author.profileImageUrl}
+                  date={comment.createdAt}
+                  content={comment.content}
+                />
+              ))}
             </div>
           </div>
-          <div className="flex flex-col gap-20">
-            {comments.comments.map((comment) => (
-              <Reply
-                key={comment.id}
-                nickname={comment.author.nickname}
-                profile={comment.author.profileImageUrl}
-                date={comment.createdAt}
-                content={comment.content}
+          <div className="flex flex-col gap-6 border-1 border-gray-d9 rounded-8 w-200 max-h-160 p-16 min-w-180 my-16">
+            <span className="text-12 font-semibold">담당자</span>
+            <div className="flex flex-row justify-start content-center gap-8">
+              <Image
+                src={cardData?.assignee.profileImageUrl || 'assets/chip/ellipseDefault.svg'}
+                width={34}
+                height={34}
+                alt="profile"
+                className="rounded-full"
               />
-            ))}
+              <span className="text-14 font-normal content-center">{cardData?.assignee.nickname}</span>
+            </div>
+            {!!cardData?.dueDate && (
+              <>
+                <span className="text-12 font-semibold">마감일</span>
+                <span className="text-14 font-normal">{dueDate}</span>
+              </>
+            )}
           </div>
         </div>
-        <div className="flex flex-col gap-6 border-1 border-gray-d9 rounded-8 w-200 max-h-160 p-16 min-w-180 my-16">
-          <span className="text-12 font-semibold">담당자</span>
-          <div className="flex flex-row justify-start content-center gap-8">
-            <Image
-              src={taskData.assignee.profileImageUrl}
-              width={34}
-              height={34}
-              alt="profile"
-              className="rounded-full"
-            />
-            <span className="text-14 font-normal content-center">{taskData.assignee.nickname}</span>
-          </div>
-          <span className="text-12 font-semibold">마감일</span>
-          <span className="text-14 font-normal">{dueDate}</span>
-        </div>
-      </div>
-    </>
-  } lowerChildren={<></>}/>
-  );
+      </Modal>
+    );
 };
