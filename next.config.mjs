bf2fa03e5c80ddd151da31042b0b8b5f82bb6763
Harry K <155596644+harrykotter@users.noreply.github.com>
2024-04-23 16:10:10 +0900

    🔧 이미지 주소 호스트 문제 해결
    이미지 업로드 시 이미지 호스트 허용으로 이미지 이슈 해결

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["sprint-fe-project.s3.ap-northeast-2.amazonaws.com"],
+    domains: ['sprint-fe-project.s3.ap-northeast-2.amazonaws.com'],
   },
 };
 
