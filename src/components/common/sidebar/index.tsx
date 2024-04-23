9d78085b8f715ee8d79a6b47c902556774d6f75b
정기환 <joelkh784@gmail.com>
2024-04-23 19:17:32 +0900

    ✨ Feat: add modal to dashboard plus icon

--- src/components/common/sidebar/index.tsx
+++ src/components/common/sidebar/index.tsx
@@ -6,6 +6,8 @@ import crown from '@/public/assets/icon/crown.svg';
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import instance from '@/src/util/axios';
+import AddDashboardModal from '@/src/components/dashboardModal/addDashboardModal';
+
 // import ReactPaginate from 'react-paginate';
 
 type Dashboard = {
@@ -97,6 +99,13 @@ const DashboardList = () => {
 };
 
 const SideBar = () => {
+  const [isAddDashboardModalVisible, setIsAddDashboardModalVisible] = useState(false);
+  const showAddDashboardModal = () => {
+    setIsAddDashboardModalVisible(true);
+  };
+  const hideAddDashboardModal = () => {
+    setIsAddDashboardModalVisible(false);
+  };
   return (
     <div className="top-0 left-0 w-300 h-screen border-1 bg-white pt-20 px-24 tablet:w-160 mobile:w-67">
       <div className="w-109 mb-60 mobile:hidden">
@@ -108,9 +117,17 @@ const SideBar = () => {
       <div className="flex justify-between">
         <p className="text-12 font-bold text-gray-78 mobile:hidden">Dash Boards</p>
         {/* <div className="w-20 mobile:mt-39 mobile:mb-22" onClick={handleOpenModal}> 대시보드 생성하기 모달 핸들러 추가 */}
-        <Image src={add} alt="add dash board" className="w-20 mobile:mt-39 mobile:mb-22 cursor-pointer" />
+        <Image
+          src={add}
+          alt="add dash board"
+          className="w-20 mobile:mt-39 mobile:mb-22 cursor-pointer"
+          onClick={showAddDashboardModal}
+        />
       </div>
       <DashboardList />
+      {isAddDashboardModalVisible && (
+        <AddDashboardModal openModal={isAddDashboardModalVisible} handleModalClose={hideAddDashboardModal} />
+      )}
     </div>
   );
 };
