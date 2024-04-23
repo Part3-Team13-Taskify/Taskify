9c47c83ddb7b1671b7b2df0cb0af9191955f8bc2
byunjiin <jibyun5808@naver.com>
2024-04-24 02:44:00 +0900

    ✨ Feat: 엑세스 토큰 interceptor에 설정

--- src/util/axios.ts
+++ src/util/axios.ts
@@ -4,8 +4,7 @@ const instance = axios.create({
   baseURL: 'https://sp-taskify-api.vercel.app/4-13/',
 });
 
-const token =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTk5MywidGVhbUlkIjoiNC0xMyIsImlhdCI6MTcxMzcxMzk0NiwiaXNzIjoic3AtdGFza2lmeSJ9.r2xKX_D4sv3R77JjPt--Wm9dGycyzusV6s9nnXtYz74';
+const token = window.localStorage.getItem('accessToken');
 
 instance.interceptors.request.use((config) => {
   const modifiedConfig = { ...config };
