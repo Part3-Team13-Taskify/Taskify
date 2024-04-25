07285bc89b0c44809704a703bd35fb8a1113976c
Harry K <155596644+harrykotter@users.noreply.github.com>
2024-04-26 00:17:45 +0900

    🐛 할일 생성 기능 삭제 오류 수정
    기존 파일명 변경으로 인한 할일 생성 기능 재구현

--- src/components/dashboard/columnList/index.tsx
+++ src/components/dashboard/columnList/index.tsx
@@ -6,8 +6,11 @@ import addLarge from '@/public/assets/chip/addLarge.svg';
 import Button from '@/src/components/common/button';
 import Violet from '@/public/assets/chip/ellipseVioletSmall.svg';
 import ScrollButton from '../scrollButton';
+import useModal from '@/src/hooks/useModal';
 import AddColumnButton from '../addColumnButton';
 import EditColumnButton from '../editColumnButton';
+import CreateTask from '../../TaskModal/CreateTask';
+import ModalPortal from '../../common/modalPortal';
 
 type Columns = {
   id: number;
@@ -18,6 +21,12 @@ type Columns = {
 };
 
 const ColumnsList: React.FC = () => {
+  const {
+    openModal: createTaskModal,
+    handleModalClose: createTaskModalClose,
+    handleModalOpen: createTaskModalOpen,
+  } = useModal();
+
   const router = useRouter();
   const { id } = router.query;
   const dashboardId = Number(id);
@@ -74,12 +83,21 @@ const ColumnsList: React.FC = () => {
                 />
               </div>
               <div>
+                <ModalPortal>
+                  <CreateTask
+                    openModal={createTaskModal}
+                    handleModalClose={createTaskModalClose}
+                    dashboardId={dashboardId}
+                    columnId={column.id}
+                  />
+                </ModalPortal>
                 <Button
                   className="tablet:w-full mobile:w-full"
                   buttonType="add"
                   bgColor="white"
                   textColor="black"
                   type="button"
+                  onClick={createTaskModalOpen}
                 >
                   <Image src={addLarge} alt="addBox" className="w-22 h-22 p-3 rounded bg-violet-8%" />
                 </Button>
