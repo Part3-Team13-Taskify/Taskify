fdcf8695c5e740a658667eff285803166f50e535
ysh73900 <74593476+ysh73900@users.noreply.github.com>
2024-04-25 13:57:39 +0900

    ✨ Feat: 컬럼추가버튼 컴포넌트화
    columnList파일이 너무 길어져서 컬럼추가버튼을 컴포넌트화 했음

---
+++ src/components/dashboard/addColumnButton/index.tsx
@@ -0,0 +1,53 @@
+import Image from 'next/image';
+import useModal from '@/src/hooks/useModal';
+import addLarge from '@/public/assets/chip/addLarge.svg';
+import Button from '../../common/button';
+import ModalPortal from '../../common/modalPortal';
+import AddColumnModal from '../../columnModal/addColumnModal';
+
+type Columns = {
+  id: number;
+  title: string;
+  teamId: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+interface AddColumnButtonProps {
+  dashboardId: number;
+  setColumnsList: React.Dispatch<React.SetStateAction<Columns[]>>;
+}
+
+const AddColumnButton: React.FC<AddColumnButtonProps> = ({ dashboardId, setColumnsList }) => {
+  const {
+    openModal: addColumnModal,
+    handleModalClose: addColumnModalClose,
+    handleModalOpen: addColumnModalOpen,
+  } = useModal();
+  return (
+    <div className="fixed right-40 bottom-40">
+      <ModalPortal>
+        <AddColumnModal
+          openModal={addColumnModal}
+          handleModalClose={addColumnModalClose}
+          dashboardId={dashboardId}
+          setColumnsList={setColumnsList}
+        />
+      </ModalPortal>
+
+      <Button
+        className="hover:bg-violet-dark"
+        buttonType="columnAdd"
+        bgColor="violet"
+        textColor="white"
+        type="button"
+        onClick={addColumnModalOpen}
+      >
+        <div className="flex items-center gap-12 tablet:hidden">새로운 컬럼추가</div>
+        <Image src={addLarge} alt="addBox" className="w-25 h-25 p-3 rounded bg-white" />
+      </Button>
+    </div>
+  );
+};
+
+export default AddColumnButton;
