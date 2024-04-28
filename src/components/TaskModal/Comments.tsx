29c6303138f0781df98d434836b39d3c567cbffa
Harry K <155596644+harrykotter@users.noreply.github.com>
2024-04-28 14:58:50 +0900

    ✨ 댓글 요청 및 생성 기능 추가
    댓글 목록 조회 및 댓글 쓰기 기능 추가

---
+++ src/components/TaskModal/Comments.tsx
@@ -0,0 +1,108 @@
+import instance from '@/src/util/axios';
+import { ChangeEventHandler, MouseEventHandler, useEffect, useState } from 'react';
+import Reply from './Reply';
+import Button from '../common/button';
+
+interface CommentData {
+  id: number;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  cardId: number;
+  author: {
+    id: number;
+    nickname: string;
+    profileImageUrl: string;
+  };
+}
+interface CommentResponse {
+  comments: CommentData[];
+  cursorId: number | null;
+}
+
+const Comments = () => {
+  const cardId = 5253;
+  const columnId = 22378;
+  const dashboardId = 6661;
+
+  const [commentList, setCommentList] = useState<CommentResponse>({ comments: [], cursorId: null });
+  const [replyValue, setReplyValue] = useState('');
+
+  const getComments = async (id: number) => {
+    const response = await instance.get(`comments?cardId=${id}`);
+    setCommentList(response.data);
+  };
+  const postComment = async (card: number, column: number, dashboard: number) => {
+    const response = await instance.post(`comments`, {
+      content: replyValue,
+      cardId: card,
+      columnId: column,
+      dashboardId: dashboard,
+    });
+    if (response.status === 201) {
+      setReplyValue('');
+    }
+  };
+
+  const handleTextChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
+    setReplyValue(e.target.value.trim());
+  };
+  const handleReplyClick: MouseEventHandler<HTMLButtonElement> = () => {
+    postComment(cardId, columnId, dashboardId);
+  };
+
+  useEffect(() => {
+    getComments(cardId);
+  });
+
+  return (
+    <>
+      <div className="gap-24">
+        <div className="flex flex-col relative">
+          <label htmlFor="reply" className="my-10">
+            댓글
+          </label>
+          <textarea
+            id="reply"
+            className="p-16 border-1 border-gray-d9 rounded-6 h-110 text-14 resize-none"
+            placeholder="댓글 작성하기"
+            value={replyValue}
+            onChange={handleTextChange}
+          />
+          <Button
+            className="absolute bottom-12 right-12"
+            buttonType="modal1"
+            type="button"
+            bgColor="white"
+            textColor="violet"
+            disabled={!!replyValue}
+          >
+            입력
+          </Button>
+          <button
+            className={`absolute bottom-12 right-12 text-12 border rounded-6 border-gray-df px-31 py-6 ${
+              replyValue ? 'bg-white text-violet' : 'bg-gray-50 text-gray-78'
+            }`}
+            disabled={!replyValue}
+            onClick={handleReplyClick}
+          >
+            입력
+          </button>
+        </div>
+      </div>
+      <div className="flex flex-col gap-20">
+        {commentList.comments.map((comment) => (
+          <Reply
+            key={comment.id}
+            nickname={comment.author.nickname}
+            profile={comment.author.profileImageUrl}
+            date={comment.createdAt}
+            content={comment.content}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Comments;
