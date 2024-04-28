f9c8189497aa5a89c7ee91765be1722ce57b6650
정기환 <joelkh784@gmail.com>
2024-04-28 12:10:51 +0900

    💄 Style: dashboard bg changes with its color when selected

--- src/components/common/sidebar/DashboardList.tsx
+++ src/components/common/sidebar/DashboardList.tsx
@@ -3,7 +3,7 @@ import DashboardListItem from './DashboardListItem';
 
 const DashboardList = () => {
   return (
-    <div className="w-full h-45 flex my-16 flex-col mobile:gap-30 mobile:translate-x-4">
+    <div className=" w-full h-45 flex my-16 flex-col mobile:gap-30 mobile:translate-x-4">
       <DashboardListItem />
       <DashboardListPagination />
     </div>
