14b800eb748b6bbf82080e9f84e5c5a38842d845
김경주 <114053155+gaeaeg@users.noreply.github.com>
2024-04-28 16:53:52 +0900

    ✨ Feat: MyDashboardList 페이지 네이션 (#66)
    * 🐛 Fix: mydashboard페이지 반응형 수정
    
    * ✨ Feat: InvitationTable UI
    
    * ✨ Feat: 초대목록 조회 API
    
    * ✨ Feat: 초대목록조회 API 연결
    
    * ✨ Feat: 초대응답 API
    
    * ✨ Feat: 초대목록 무한스크롤 구현
    
    * ✨ Feat: 초대응답 API 연결
    
    * ✨ Feat: 초대목록 유무에 따라 UI 다르게
    
    * ✨ Feat: conflict
    
    * ✨ Feat: 대시보드 목록 API
    
    * ✨ Feat: MyDashboardList 페이지 네이션
    
    * ♻️ Refactor: grid로 수정
    
    * ♻️ Refactor: ModalPortal 추가
    
    * ✨ Feat: InvitationTable UI
    
    * ✨ Feat: 초대목록조회 API 연결
    
    * ✨ Feat: 대시보드 목록 API
    
    * ✨ Feat: MyDashboardList 페이지 네이션
    
    * ♻️ Refactor: grid로 수정
    
    * ♻️ Refactor: ModalPortal 추가

---
+++ src/components/mydashboard/MyDashboardList.tsx
@@ -0,0 +1,92 @@
+import { useMyDashboardListStore } from '@/src/util/zustand';
+import React, { useCallback, useEffect, useState } from 'react';
+import { fetchDashboardList } from '@/src/pages/api/dashboardEditApi';
+import type { Dashboard } from '@/src/util/zustand';
+import MyDashboardPagination from '@/src/components/mydashboard/MyDashboardPagination';
+import MyDashboardListItem from '@/src/components/mydashboard/MyDashboardListItem';
+import Button from '@/src/components/common/button';
+import addLarge from '@/public/assets/chip/addLarge.svg';
+import AddDashboardModal from '@/src/components/dashboardModal/addDashboardModal';
+import Image from 'next/image';
+
+const DashboardList = () => {
+  const [selectedDashboard, setSelectedDashboard] = useState(0);
+  const dashboardList = useMyDashboardListStore((state) => state.myDashboardList);
+  const setMyDashboardList = useMyDashboardListStore((state) => state.setMyDashboardList);
+  const [isAddDashboardModalVisible, setIsAddDashboardModalVisible] = useState(false);
+  const [totalCount, setTotalCount] = useState(0);
+  const [offset, setOffset] = useState(1);
+  const itemsPerPage = 5;
+  const maxOffset = Math.ceil(totalCount / itemsPerPage);
+
+  const showAddDashboardModal = () => {
+    setIsAddDashboardModalVisible(true);
+  };
+
+  const hideAddDashboardModal = () => {
+    setIsAddDashboardModalVisible(false);
+  };
+
+  const handleClick = useCallback((dashboardId: number) => {
+    setSelectedDashboard(dashboardId);
+  }, []);
+
+  const updatePageInfo = useCallback(
+    (newOffset) => {
+      fetchDashboardList(newOffset, itemsPerPage).then((res) => {
+        setMyDashboardList(res.dashboards);
+        setTotalCount(res.totalCount);
+        setOffset(newOffset);
+      });
+    },
+    [itemsPerPage, setMyDashboardList],
+  );
+
+  useEffect(() => {
+    updatePageInfo(offset);
+  }, [offset, updatePageInfo]);
+
+  const handlePageChange = (newOffset) => {
+    setOffset(newOffset);
+  };
+
+  useEffect(() => {
+    updatePageInfo(1);
+  }, [updatePageInfo]);
+
+  return (
+    <div className="flex-col mobile:w-full">
+      <div className="grid grid-cols-3 gap-12 my-16 tablet:grid-cols-2 tablet:gap-10 mobile:flex mobile:flex-col mobile:gap-8">
+        <Button
+          className="mobile:w-full"
+          buttonType="dashboardAdd"
+          bgColor="white"
+          textColor="black"
+          type="button"
+          onClick={showAddDashboardModal}
+        >
+          새로운 대시보드
+          <Image src={addLarge} alt="addBox" className="w-22 h-22 p-3 rounded bg-violet-8%" />
+        </Button>
+        {isAddDashboardModalVisible && (
+          <AddDashboardModal openModal={isAddDashboardModalVisible} handleModalClose={hideAddDashboardModal} />
+        )}
+        {Array.isArray(dashboardList) &&
+          dashboardList.map((data: Dashboard) => (
+            <MyDashboardListItem
+              key={data.id}
+              data={data}
+              handleClick={handleClick}
+              selectedDashboard={selectedDashboard}
+            />
+          ))}
+      </div>
+      <div className="flex justify-end items-center gap-18">
+        <div>{`${maxOffset} 페이지 중 ${offset}`}</div>
+        <MyDashboardPagination offset={offset} setOffset={handlePageChange} />
+      </div>
+    </div>
+  );
+};
+
+export default DashboardList;
---
+++ src/components/mydashboard/MyDashboardListItem.tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+import crown from '@/public/assets/icon/crown.svg';
+import arrowReverse from '@/public/assets/icon/arrowReverse.svg';
+import Image from 'next/image';
+import type { Dashboard } from '@/src/util/zustand';
+import React from 'react';
+
+type DashboardListItemProps = {
+  data: Dashboard;
+  handleClick: (dashboardId: number) => void;
+  selectedDashboard: number;
+};
+
+const MyDashboardListItem = React.memo(({ data, handleClick, selectedDashboard }: DashboardListItemProps) => (
+  <Link href={`/dashboard/${data.id}`} key={data.id}>
+    <div
+      key={data.id}
+      className="flex justify-between px-20 items-center w-332 h-70 rounded-8 bg-white border-gray-d9 border-1 tablet:w-247 tablet:h-68 mobile:w-full mobile:h-58 "
+      role="button"
+      tabIndex={0}
+      onClick={() => handleClick(data.id)}
+    >
+      <div className="flex items-center">
+        <div style={{ backgroundColor: data.color }} className="w-8 h-8 rounded-99 flex-shrink-0" />
+        <p
+          className={`text-16 font-semibold ml-16 mr-8 tablet:ml-12 tablet:mr-6 mobile:text-14 mobile:ml-12 mobile:mr-4 ${
+            data.id === selectedDashboard && ' text-violet'
+          }`}
+        >
+          {data.title}
+        </p>
+        {data.createdByMe && <Image src={crown} alt="crown" width={18} className="-translate-y-1" />}
+      </div>
+      <Image src={arrowReverse} alt="arrowReverse" />
+    </div>
+  </Link>
+));
+
+export default MyDashboardListItem;
