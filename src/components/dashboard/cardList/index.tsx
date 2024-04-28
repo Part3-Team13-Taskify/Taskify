3ae9a84dc4a1a7a896b6a7bfae4feffd29d637b1
ysh73900 <74593476+ysh73900@users.noreply.github.com>
2024-04-28 23:53:33 +0900

    ✨ CardList 무한스크롤 구현

--- src/components/dashboard/cardList/index.tsx
+++ src/components/dashboard/cardList/index.tsx
@@ -1,7 +1,7 @@
-import instance from '@/src/util/axios';
+import { useEffect, useRef, useState } from 'react';
+import { useCardId, useCardListStore } from '@/src/util/zustand';
 import useModal from '@/src/hooks/useModal';
-import { useEffect, useState } from 'react';
-import { useCardId } from '@/src/util/zustand';
+import { getCardList, getCardListAdditional } from '@/src/pages/api/cardListApi';
 import Card from '../../common/card';
 import ModalPortal from '../../common/modalPortal';
 import { TaskCard } from '../../TaskModal/TaskCard';
@@ -28,18 +28,85 @@ interface CardListType {
 }
 
 const CardList = ({ columnId, title }: { columnId: number; title: string }) => {
-  const [cardList, setCards] = useState<CardListType>();
+  // const [cardList, setCardList] = useState<CardListType>({ cards: [], totalCount: 0, cursorId: null });
+  const [hasMoreItems, setHasMoreItems] = useState<boolean>(true);
+  const observerRef = useRef<HTMLDivElement>(null);
 
-  const getCardList = async () => {
-    const response = await instance.get(`cards?columnId=${columnId}`);
-    setCards(response.data);
-  };
   const { openModal: taskModal, handleModalClose: TaskModalClose, handleModalOpen: TaskModalOpen } = useModal();
   const cardId = useCardId((state) => state.cardId);
+  const { cardLists, setCardList } = useCardListStore();
+  const cardList = cardLists[columnId] || { cards: [], totalCount: 0, cursorId: null };
+
+  // 카드리스트 불러오기
+  const getCards = async () => {
+    try {
+      const response = await getCardList({ column: columnId });
+      const cardsData = response.cards;
+
+      const newCardList: CardListType = {
+        cards: [...cardList.cards, ...cardsData],
+        totalCount: response.totalCount,
+        cursorId: response.cursorId,
+      };
+
+      setCardList(columnId, newCardList);
+      setHasMoreItems(cardsData.length > 0);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // 추가적으로 카드리스트 불러오기
+  const getCardsAdditional = async () => {
+    if (cardList.cursorId) {
+      try {
+        const response = await getCardListAdditional({ column: columnId, targetId: cardList.cursorId });
+        const cardsData = response.cards;
+
+        const newCardList: CardListType = {
+          cards: [...cardList.cards, ...cardsData],
+          totalCount: response.totalCount,
+          cursorId: response.cursorId,
+        };
+        setCardList(columnId, newCardList);
+        setHasMoreItems(cardsData.length > 0);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
 
   useEffect(() => {
-    getCardList();
-  }, [columnId]);
+    getCards();
+  }, []);
+
+  // 무한스크롤
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting && hasMoreItems) {
+            getCardsAdditional();
+          }
+        });
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 1.0,
+      },
+    );
+
+    if (observerRef.current) {
+      observer.observe(observerRef.current);
+    }
+
+    return () => {
+      if (observerRef.current) {
+        observer.unobserve(observerRef.current);
+      }
+    };
+  }, [cardList.cursorId, hasMoreItems]);
 
   return (
     <>
@@ -60,6 +127,7 @@ const CardList = ({ columnId, title }: { columnId: number; title: string }) => {
           />
         );
       })}
+      <div ref={observerRef} />
     </>
   );
 };
