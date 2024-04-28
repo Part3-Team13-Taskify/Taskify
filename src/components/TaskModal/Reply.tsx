edb5c8db22f31fd0cd6b29f6f1c1581d59e2051e
Harry K <155596644+harrykotter@users.noreply.github.com>
2024-04-28 20:23:01 +0900

    🚨 Eslint 요류 수정
    eslint 오류 수정

--- src/components/TaskModal/Reply.tsx
+++ src/components/TaskModal/Reply.tsx
@@ -1,5 +1,7 @@
+import instance from '@/src/util/axios';
 import { format } from 'date-fns';
 import Image from 'next/image';
+import { ChangeEventHandler, useState } from 'react';
 
 interface Props {
   nickname: string;
@@ -10,20 +12,61 @@ interface Props {
 
 const Reply = ({ nickname: name, profile, date, content: replyContent }: Props) => {
   const formattedDate = format(new Date(date), 'yyyy.MM.dd HH:mm');
+  const [isEditting, setIsEditting] = useState<boolean>(false);
+  const [replyValue, setReplyValue] = useState<string>(replyContent);
+
+  // const deleteComment = async () => {
+  //   await instance.delete(`comments/`);
+  // };
+
+  const handleEditCommentClick = () => {
+    setIsEditting(true);
+  };
+  const handleEditCancel = () => {
+    setIsEditting(false);
+    setReplyValue(replyContent);
+  };
+  const handleReplyChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
+    setReplyValue(e.target.value.trim());
+  };
+
   return (
-    <div className="flex flex-row gap-8">
+    <div className="flex flex-row gap-8 w-full">
       <div>
         <Image src={profile} width={34} height={34} alt="profile" className="content-center rounded-99" />
       </div>
-      <div>
+      <div className="flex flex-col gap-4 w-full">
         <div className="flex flex-row gap-8">
           <div className="text-14 font-semibold">{name}</div>
           <div className="text-12 font-normal text-gray-9f">{formattedDate}</div>
         </div>
-        <div className="text-14 font-normal">{replyContent}</div>
+        {isEditting ? (
+          <div className="relative w-full">
+            <textarea className="w-full resize-none" value={replyValue} onChange={handleReplyChange} />
+            <div className="flex flex-row gap-4 absolute bottom-12 right-12 ">
+              <button
+                className="text-12 border rounded-6 border-gray-df px-31 py-6 bg-white text-black"
+                onClick={handleEditCancel}
+              >
+                취소
+              </button>
+              <button
+                className={`text-12 border rounded-6 border-gray-df px-31 py-6 ${
+                  replyContent ? 'bg-white text-violet' : 'bg-gray-50 text-gray-78'
+                }`}
+              >
+                수정
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div className="text-14 font-normal">{replyContent}</div>
+        )}
         <div className="flex flex-row gap-12 text-12 text-gray-9f">
-          <div className="underline">수정</div>
-          <div className="underline">삭제</div>
+          <button className="underline" onClick={handleEditCommentClick}>
+            수정
+          </button>
+          <button className="underline">삭제</button>
         </div>
       </div>
     </div>
